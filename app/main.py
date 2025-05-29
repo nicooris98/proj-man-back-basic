fff2d7fe2600b91c7460dac91518976b37277a9f
Nicolas Ysidoro Oris <nicolasoris98@gmail.com>
2025-05-29 13:14:52 -0300

    hotfix

--- app/main.py
+++ app/main.py
@@ -31,3 +31,8 @@ app.add_middleware(
     allow_methods=["*"],  
     allow_headers=["*"],
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
